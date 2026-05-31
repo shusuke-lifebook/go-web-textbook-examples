a1cfbb91262c435efc7c481c872c7207e8ef8b99
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-05-31 14:59:05 +0900

    ginのサンプルを追加

---
+++ ch01-gin-intro/cmd/api/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"ch01-gin-intro/ch01-gin-intro/internal/router"
+	"log/slog"
+	"os"
+)
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	r := router.New()
+	if err := r.Run(":8080"); err != nil {
+		logger.Error("server exited", "error", err)
+		os.Exit(1)
+	}
+}
---
+++ ch01-gin-intro/internal/router/router.go
@@ -0,0 +1,15 @@
+// Package router
+package router
+
+import (
+	"ch01-gin-intro/ch01-gin-intro/internal/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+// New はルーティング設定済みのエンジンを返す
+func New() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", handler.Ping)
+	return r
+}
