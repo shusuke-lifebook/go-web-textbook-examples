de402236396eb23ff69286974887dbe085944a7e
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2026-06-20 09:49:36 +0900

    apperrorの作成

---
+++ ch05-validation/internal/apperror/apperror.go
@@ -0,0 +1,26 @@
+// Package apperror
+package apperror
+
+// FieldIssueはフィールド単位のバリデーション詳細
+type FieldIssue struct {
+	Field   string `json:"field"`
+	Tag     string `json:"tag"`
+	Param   string `json:"param,omitempty"`
+	Message string `json:"message"`
+}
+
+// AppErrorはHTTP境界で扱うエラー表現
+type AppError struct {
+	Code     string       // 安定コード： "VALIDATION_FAILED"等
+	Message  string       // 利用者向けメッセージ
+	HTTPCode int          // HTTPステータス
+	Cause    error        // 下位エラー(%w相当)
+	Details  []FieldIssue // バリデーション詳細
+}
+
+func (e *AppError) Error() string {
+	if e.Cause != nil {
+		return e.Code + ": " + e.Message + "(" + e.Cause.Error() + ")"
+	}
+	return e.Code + ": " + e.Message
+}
